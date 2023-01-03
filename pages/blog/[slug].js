55ef6b99e2d062f589d5b368732a706268b574aa
brandonbrown <brandon@centraflux.com>
2023-01-02 23:50:53 -0500

    update slug

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -21,7 +21,7 @@ export async function getStaticPaths(){
 
     const paths = files.map(filename => ({
         params: {
-            slug: filename.replace('md', '')
+            slug: filename.replace('.md', '')
         }
     }))
 
